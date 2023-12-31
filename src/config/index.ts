8fefa70c17dd310241e61bc60d64964f8163282c
georgeprima5 <eastonlin929@gmail.com>
2023-12-31 21:49:49 +0800

    231231-Cart

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI_Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/nav/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestsellers",
+        href: "#",
+        imageSrc: "/nav/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "Icons" as const,
+    featured: [
+      {
+        name: "Editor picks",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      {
+        name: "New Arrivals",
+        href: "#",
+        imageSrc: "/nav/icons/new.jpg",
+      },
+      {
+        name: "Bestsellering Icons",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
---
+++ src/hooks/use-on-click-outside.tsx
@@ -0,0 +1,24 @@
+import { RefObject, useEffect } from "react";
+
+type Event = MouseEvent | TouchEvent;
+
+export const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void
+) => {
+  useEffect(() => {
+    const listener = (event: Event) => {
+      const e1 = ref?.current;
+      if (!e1 || e1.contains((event?.target as Node) || null)) {
+        return;
+      }
+      handler(event);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, handler]);
+};
